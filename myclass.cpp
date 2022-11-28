98dfe343342f600b8e3a8115f0c3f7d7917edd24
waqarkhosa <waqarkhosa7@gmail.com>
2022-11-28 10:53:59 +0500

    added

--- myclass.cpp
+++ myclass.cpp
@@ -39,7 +39,7 @@ class MyClass {
     }
     while (exit != 'E' && exit != 'e');
     
-        cout<<"You have exited & Enter to myvowel function: \n";
+        cout<<"You have exited & Enter in myvowel function: \n";
         
     }
     void myvowel(){
